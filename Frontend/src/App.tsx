7b8c7601d38e4fea9b00ece947cd0f903805d57f
Nahili <nahili.ansha@mnsu.edu>
2025-12-19 11:15:50 -0800

    Signup-page

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.tsx
+import Signup from "./components/Signup";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Signup />
+    </div>
+  );
+}
